7377118f2a4ff70218642a874b5fc90302da73d1
nikita <nikita.bobers@gmail.com>
2020-12-01 23:59:33 +0200

    Warning message component

---
+++ src/components/slider/common/WarningMessage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WarningMessage = () => {
+	return (
+		<p style={{ color: "red", fontWeight: "bold" }}>
+			Please insert more items in slider component!
+		</p>
+	);
+};
+
+export { WarningMessage };

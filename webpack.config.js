92ffc331b49001f4d27da01a3d47b37a2b3a2035
nikita <nikita.bobers@gmail.com>
2020-12-10 22:01:24 +0200

    Fixed images issue in slider component

--- webpack.config.js
+++ webpack.config.js
@@ -4,44 +4,52 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	// React entry file
-	entry: "./src/index.jsx",
+  // React entry file
+  entry: "./src/index.jsx",
 
-	// Compiled react app in single JS file
-	output: {
-		path: path.join(__dirname, "/dist"),
-		filename: "index_bundle.js",
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader",
-				},
-			},
+  // Compiled react app in single JS file
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index_bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
 
-			// Allow import css files in react components
-			{
-				test: /\.css$/,
-				use: ["style-loader", "css-loader"],
-			},
-		],
-	},
-	devServer: {
-		contentBase: path.join(__dirname, "dist"),
-		compress: true,
-		port: 3000,
-		hot: true,
-		open: true,
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "./src/index.html",
-		}),
-	],
-	resolve: {
-		extensions: [".js", ".jsx"],
-	},
+      // Allow import css files in react components
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+
+      // Images setup
+      {
+        test: /\.(jpeg|jpg|png)$/,
+        use: {
+          loader: "url-loader",
+        },
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 3000,
+    hot: true,
+    open: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
 };

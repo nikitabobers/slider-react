a6607c2275925e85be7348c9d1ae786e45bcf1db
nikita <nikita.bobers@gmail.com>
2020-12-12 22:21:08 +0200

    Controled amount of slides to display

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,94 +1,119 @@
-import React, { createRef, useEffect } from "react";
+import React, { createRef, useEffect, useState } from "react";
 import { WarningMessage } from "./common/WarningMessage";
 import "./slider.css";
 
-const Slider = ({ children }) => {
-	// Ensure that slider have items to slide
-	if (children === undefined || children.length === undefined)
-		return (
-			<div className="container">
-				<WarningMessage />
-			</div>
-		);
+const calcItemWidth = (number, container, item, handleChange) => {
+  const containerWidth = container.current.getBoundingClientRect().width;
 
-	// Slider container (visible frame)
-	const containerRef = createRef();
-	// List container
-	const listRef = createRef();
-	// List child
-	const itemRef = createRef();
+  const margin = parseInt(
+    window.getComputedStyle(item.current).getPropertyValue("margin-left")
+  );
 
-	let isDown = false;
-	// Point coordinate in slider component on click
-	let pointClick;
-	// Current point coordinate in slider component
-	let pointCurrent;
+  const marginValue = margin / number;
 
-	// Calculate list container  width
-	useEffect(() => {
-		listRef.current.style.width = `${
-			children.length * itemRef.current.clientWidth
-		}px`;
-	}, []);
+  handleChange(containerWidth / number - marginValue);
+};
 
-	//
-	// Mouse events
-	//
+const Slider = ({ children, slidesDisplayed }) => {
+  // Ensure that slider have items to slide
+  if (children === undefined || children.length === undefined)
+    return (
+      <div className="container">
+        <WarningMessage />
+      </div>
+    );
 
-	const mouseDown = (e) => {
-		isDown = true;
-		pointClick = e.pageX - containerRef.current.offsetLeft;
-		pointCurrent = containerRef.current.scrollLeft;
-	};
+  // Slider container (visible frame)
+  const containerRef = createRef();
+  // List container
+  const listRef = createRef();
+  // List child
+  const itemRef = createRef();
 
-	const mouseLeave = () => (isDown = false);
-	const mouseUp = () => (isDown = false);
+  let isDown = false;
+  // Point coordinate in slider component on click
+  let pointClick;
+  // Current point coordinate in slider component
+  let pointCurrent;
 
-	const mouseMove = (e) => {
-		if (!isDown) return;
-		e.preventDefault();
-		const startPoint = e.pageX - containerRef.current.offsetLeft;
-		const move = startPoint - pointClick;
-		containerRef.current.scrollLeft = pointCurrent - move;
-	};
+  // list item width
+  const [itemWidth, setItemWidth] = useState(0);
 
-	//
-	// Touch screen events
-	//
+  // Calculate slide width
+  useEffect(() => {
+    calcItemWidth(slidesDisplayed, containerRef, itemRef, setItemWidth);
+  }, []);
 
-	const touchStart = (e) => {
-		pointClick = e.touches[0].clientX - containerRef.current.offsetLeft;
-		pointCurrent = containerRef.current.scrollLeft;
-	};
+  // Calculate list container  width
+  useEffect(() => {
+    listRef.current.style.width = `${children.length * itemWidth}px`;
+  }, [itemWidth]);
 
-	const touchMove = (e) => {
-		const startPoint = e.touches[0].clientX - containerRef.current.offsetLeft;
-		const move = startPoint - pointClick;
-		containerRef.current.scrollLeft = pointCurrent - move;
-	};
+  //
+  // Mouse events
+  //
 
-	return (
-		<div className="container">
-			<div className="slider__container" ref={containerRef}>
-				<ul
-					className={`slider__list`}
-					ref={listRef}
-					onMouseDown={(e) => mouseDown(e)}
-					onMouseLeave={(e) => mouseLeave(e)}
-					onMouseUp={(e) => mouseUp(e)}
-					onMouseMove={(e) => mouseMove(e)}
-					onTouchStart={(e) => touchStart(e)}
-					onTouchMove={(e) => touchMove(e)}
-				>
-					{children.map((item, index) => (
-						<li ref={itemRef} className="slider__item" key={index}>
-							{item}
-						</li>
-					))}
-				</ul>
-			</div>
-		</div>
-	);
+  const mouseDown = (e) => {
+    isDown = true;
+    pointClick = e.pageX - containerRef.current.offsetLeft;
+    pointCurrent = containerRef.current.scrollLeft;
+  };
+
+  const mouseLeave = () => (isDown = false);
+  const mouseUp = () => (isDown = false);
+
+  const mouseMove = (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const startPoint = e.pageX - containerRef.current.offsetLeft;
+    const move = startPoint - pointClick;
+    console.log(pointCurrent - move);
+    containerRef.current.scrollLeft = pointCurrent - move;
+    // containerRef.current.scrollLeft += 10;
+  };
+
+  //
+  // Touch screen events
+  //
+
+  const touchStart = (e) => {
+    pointClick = e.touches[0].clientX - containerRef.current.offsetLeft;
+    pointCurrent = containerRef.current.scrollLeft;
+  };
+
+  const touchMove = (e) => {
+    const startPoint = e.touches[0].clientX - containerRef.current.offsetLeft;
+    const move = startPoint - pointClick;
+    // containerRef.current.scrollLeft = pointCurrent - move;
+  };
+
+  return (
+    <div className="container">
+      <div className="slider__container" ref={containerRef}>
+        <ul
+          className={`slider__list`}
+          ref={listRef}
+          onMouseDown={(e) => mouseDown(e)}
+          onMouseLeave={(e) => mouseLeave(e)}
+          onMouseUp={(e) => mouseUp(e)}
+          onMouseMove={(e) => mouseMove(e)}
+          onTouchStart={(e) => touchStart(e)}
+          onTouchMove={(e) => touchMove(e)}
+        >
+          {children.map((item, index) => (
+            <li
+              ref={itemRef}
+              style={{ width: itemWidth }}
+              className="slider__item"
+              key={index}
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export { Slider };
